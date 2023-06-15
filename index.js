68e7749bdda35b01c258e9e655500c5211bbf58f
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2023-06-15 14:39:42 +0530

    randomImages Based on randomVar

---
+++ index.js
@@ -0,0 +1,7 @@
+var randomvar1 = Math.floor(Math.random() * 6) + 1; //generate random var between 1 and 6
+var randomvar2 = Math.floor(Math.random() * 6) + 1; 
+
+var randomDiceImages = "./images/dice"+ randomvar1 +".png"; //give path of dice images based on randomVar1
+var randomDiceImages = "./images/dice"+ randomvar2 +".png"; //give path of dice images based on randomVar2
+
+
