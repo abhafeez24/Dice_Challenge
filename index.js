73d000706fc37e863d2f9f84c6fc8f58a9ccdb17
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2023-06-15 14:49:31 +0530

    Worked on dice Images

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var randomvar1 = Math.floor(Math.random() * 6) + 1; //generate random var between 1 and 6
 var randomvar2 = Math.floor(Math.random() * 6) + 1; 
 
-var randomDiceImages = "./images/dice"+ randomvar1 +".png"; //give path of dice images based on randomVar1
-var randomDiceImages = "./images/dice"+ randomvar2 +".png"; //give path of dice images based on randomVar2
+var randomDiceImagePlayer1 = "./images/dice"+ randomvar1 +".png"; //images/dice1.png - images/dice6.png
+var randomDiceImagePlayer2 = "./images/dice"+ randomvar2 +".png"; 
 
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImagePlayer1);  //changeing player 1 dice
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImagePlayer2);  //changeing player 1 dice
 
